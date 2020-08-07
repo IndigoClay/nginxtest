240d8a380c3dfc901fa9d11a2b41efd3433863ab
indigoclay <indigoclay@gmail.com>
2020-08-07 15:00:47 +0200

    changed port

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello Kuh!!!</h1>');
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
